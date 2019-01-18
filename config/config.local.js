29a0dbbc23d3f38cd4deb26e48008263dc617195
chengyufeng <541318591@qq.com>
2019-01-18 17:15:47 +0800

    modify config

--- config/config.local.js
+++ config/config.local.js
@@ -50,44 +50,44 @@ module.exports = appInfo => {
     //         //     },
     //         // },
     //     ]
+    // }
 
-        config.sequelize = {
-            datasources:[
-                {
-                    dialect: 'mysql', 
-                    database: 'sj_nike_stock',
-                    host: '192.168.1.121',
-                    port: '3306',
-                    username: 'song',
-                    password: 'SongAbc12345',
-                    delegate: 'SjNikeStock',
-                    baseDir:'model/sj-nike-stock',
-                    define: {
-                        timestamps:false,
-                        freezeTableName: true,
-                    },
+    config.sequelize = {
+        datasources:[
+            {
+                dialect: 'mysql', 
+                database: 'sj_nike_stock',
+                host: '192.168.1.121',
+                port: '3306',
+                username: 'song',
+                password: 'SongAbc12345',
+                delegate: 'SjNikeStock',
+                baseDir:'model/sj-nike-stock',
+                define: {
+                    timestamps:false,
+                    freezeTableName: true,
                 },
-                
-                {
-                    dialect: 'mysql', 
-                    database: 'sj-resource',
-                    host: '192.168.1.121',
-                    port: '3306',
-                    username: 'song',
-                    password: 'SongAbc12345',
-                    delegate: 'SjResource',
-                    baseDir:'model/sj-resource',
-                    define: {
-                        timestamps:false,
-                        freezeTableName: true,
-                    },
+            },
+            
+            {
+                dialect: 'mysql', 
+                database: 'sj-resource',
+                host: '192.168.1.121',
+                port: '3306',
+                username: 'song',
+                password: 'SongAbc12345',
+                delegate: 'SjResource',
+                baseDir:'model/sj-resource',
+                define: {
+                    timestamps:false,
+                    freezeTableName: true,
                 },
-            ]
-
-        }
-
+            },
+        ]
 
     }
+
+
 
     return config;
 };
