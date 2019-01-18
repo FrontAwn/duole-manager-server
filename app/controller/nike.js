04f635f33c64c91cf5c819a991b31d4c99e527f2
chengyufeng <541318591@qq.com>
2019-01-18 15:55:17 +0800

    modify nike stock export

--- app/controller/nike.js
+++ app/controller/nike.js
@@ -2,8 +2,9 @@ const Controller = require('egg').Controller;
 const common = require('../utils/utils').common
 const promisify = require("util").promisify
 const fs = require("fs")
-const execFile = require("child_process").execFile
-const cmd = promisify(execFile)
+const child_process = require("child_process")
+const execFile = promisify(child_process.execFile)
+const exec = promisify(child_process.exec)
 const readFile = promisify(fs.readFile);
 const urlencode = require('urlencode');
 const moment = require("moment")
@@ -19,7 +20,7 @@ class NikeController extends Controller {
 	}
 
 	async exportStock() {
-		await cmd("node",['/node/duole-erp-cli/nikeToXls.js'],{maxBuffer:200*1024*1024});
+		await exec("node /node/duole-erp-cli/nikeToXls.js",{maxBuffer:200*1024*1024})
 		let excelBuffer = await readFile("/node/duole-erp-cli-local/excel/output/库存.xlsx")
 		let currentDayDate = moment().format('YYYYMMDD');
 		let excelName = `Nike库存${currentDayDate}.xlsx`
