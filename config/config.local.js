56bcb65d2ef1bcb897532d0ed5cc006bdd53763d
chengyufeng <541318591@qq.com>
2019-01-18 17:10:13 +0800

    modify config

--- config/config.local.js
+++ config/config.local.js
@@ -3,38 +3,90 @@
 module.exports = appInfo => {
     const config = exports = {};
 
-    config.sequelize = {
-        datasources:[
-            {
-                dialect: 'mysql', 
-                database: 'sj_nike_stock',
-                host: 'localhost',
-                port: '3306',
-                username: 'root',
-                password: '123456',
-                delegate: 'SjNikeStock',
-                baseDir:'model/sj-nike-stock',
-                define: {
-                    timestamps:false,
-                    freezeTableName: true,
-                },
-            },
+    // config.sequelize = {
+    //     datasources:[
+    //         {
+    //             dialect: 'mysql', 
+    //             database: 'sj_nike_stock',
+    //             host: 'localhost',
+    //             port: '3306',
+    //             username: 'root',
+    //             password: '123456',
+    //             delegate: 'SjNikeStock',
+    //             baseDir:'model/sj-nike-stock',
+    //             define: {
+    //                 timestamps:false,
+    //                 freezeTableName: true,
+    //             },
+    //         },
 
-            {
-                dialect: 'mysql', 
-                database: 'sj-resource',
-                host: 'localhost',
-                port: '3306',
-                username: 'root',
-                password: '123456',
-                delegate: 'SjResource',
-                baseDir:'model/sj-resource',
-                define: {
-                    timestamps:false,
-                    freezeTableName: true,
+    //         {
+    //             dialect: 'mysql', 
+    //             database: 'sj-resource',
+    //             host: 'localhost',
+    //             port: '3306',
+    //             username: 'root',
+    //             password: '123456',
+    //             delegate: 'SjResource',
+    //             baseDir:'model/sj-resource',
+    //             define: {
+    //                 timestamps:false,
+    //                 freezeTableName: true,
+    //             },
+    //         },
+
+    //         // {
+    //         //     dialect: 'mysql', 
+    //         //     database: 'sj_nike_stock',
+    //         //     host: '192.168.1.121',
+    //         //     port: '3306',
+    //         //     username: 'song',
+    //         //     password: 'SongAbc12345',
+    //         //     delegate: 'SjNikeStock',
+    //         //     baseDir:'model/sj-nike-stock',
+    //         //     define: {
+    //         //         timestamps:false,
+    //         //         freezeTableName: true,
+    //         //     },
+    //         // },
+    //     ]
+
+        config.sequelize = {
+            datasources:[
+                {
+                    dialect: 'mysql', 
+                    database: 'sj_nike_stock',
+                    host: '192.168.1.121',
+                    port: '3306',
+                    username: 'song',
+                    password: 'SongAbc12345',
+                    delegate: 'SjNikeStock',
+                    baseDir:'model/sj-nike-stock',
+                    define: {
+                        timestamps:false,
+                        freezeTableName: true,
+                    },
                 },
-            },
-        ]
+                
+                {
+                    dialect: 'mysql', 
+                    database: 'sj-resource',
+                    host: '192.168.1.121',
+                    port: '3306',
+                    username: 'song',
+                    password: 'SongAbc12345',
+                    delegate: 'SjResource',
+                    baseDir:'model/sj-resource',
+                    define: {
+                        timestamps:false,
+                        freezeTableName: true,
+                    },
+                },
+            ]
+
+        }
+
+
     }
 
     return config;
