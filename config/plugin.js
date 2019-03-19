106d124655711e001cf0348e3130c335e8fef57a
chengyufeng <541318591@qq.com>
2019-03-19 10:39:41 +0800

    add config

---
+++ config/plugin.js
@@ -0,0 +1,31 @@
+'use strict';
+
+exports.multipart = {
+  enable: false,
+  package: 'egg-multipart',
+};
+
+exports.onerror = {
+  enable: true,
+  package: 'egg-onerror',
+};
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
+};
+
+exports.bodyParser = {
+  enable: true,
+  package: 'body-parser',
+};
+
+exports.redis = {
+  enable: true,
+  package: 'egg-redis',
+};
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
+};
